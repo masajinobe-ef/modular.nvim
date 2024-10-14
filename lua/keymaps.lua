0e4cf2522fc477b4ecfb394c98367b09851610e0
masajinobe-ef <priscilla.effects@gmail.com>
2024-10-14 07:16:37 +0500

    up

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,10 +1,54 @@
--- Function to show notification after action
+-- [[ Functions ]]
+
+-- Show notification after action function
 local function notify_action(action_desc)
     vim.notify = require 'notify'
     vim.notify(action_desc .. ' ', vim.log.levels.INFO)
 end
 
--- [[ Basic Keymaps ]]
+-- Replace words function
+local function replace_all()
+    local replacement = vim.fn.input 'Replace with: '
+    if replacement ~= '' then
+        local current_word = vim.fn.expand '<cword>'
+        local command = string.format('%%s/%s/%s/g', current_word, replacement)
+        vim.cmd(command)
+        print 'All substitutions done'
+    else
+        print 'No replacement provided'
+    end
+end
+
+local function replace_each()
+    local replacement = vim.fn.input 'Replace with: '
+    if replacement ~= '' then
+        local current_word = vim.fn.expand '<cword>'
+        local command =
+            string.format('%%s/%s/%s/gc', current_word, replacement)
+        vim.cmd(command)
+        print 'Substitution done'
+    else
+        print 'No replacement provided'
+    end
+end
+
+-- [[ Keymaps ]]
+
+-- -----------------------------
+-- Replace words
+-- -----------------------------
+
+vim.keymap.set('n', '<leader>ra', replace_all, {
+    noremap = true,
+    silent = true,
+    desc = '[r]eplace [a]ll words',
+})
+
+vim.keymap.set('n', '<leader>re', replace_each, {
+    noremap = true,
+    silent = true,
+    desc = '[r]eplace [e]ach words with a confirmation',
+})
 
 -- -----------------------------
 -- Terminal Management
@@ -14,13 +58,13 @@ vim.keymap.set(
     'n',
     '<leader>tv',
     ':ToggleTerm direction=vertical<CR>',
-    { noremap = true, silent = true, desc = 'Terminal: Vertical' }
+    { noremap = true, silent = true, desc = '[t]erminal: [v]ertical' }
 )
 vim.keymap.set(
     'n',
     '<leader>th',
     ':ToggleTerm direction=horizontal<CR>',
-    { noremap = true, silent = true, desc = 'Terminal: Horizontal' }
+    { noremap = true, silent = true, desc = '[t]erminal: [h]orizontal' }
 )
 
 -- -----------------------------
@@ -28,40 +72,34 @@ vim.keymap.set(
 -- -----------------------------
 
 -- Save file with notification
-vim.keymap.set('n', '<C-s>', function()
+vim.keymap.set('n', '<leader>sf', function()
     vim.cmd 'w' -- Save file
     notify_action 'File saved' -- Notify after saving
-end, { noremap = true, silent = true, desc = 'Save file' })
+end, { noremap = true, silent = true, desc = '[s]ave [f]ile' })
 
 -- Quit Neovim
 vim.keymap.set(
     'n',
     '<C-q>',
     '<cmd> q <CR>',
-    { noremap = true, silent = true, desc = 'Quit Neovim' }
+    { noremap = true, silent = true, desc = '[q]uit neovim' }
 )
 
 -- -----------------------------
--- Deleting and Searching
+-- Searching
 -- -----------------------------
-
--- Delete single character without copying into register
-vim.keymap.set('n', 'x', '"_x', {
-    noremap = true,
-    silent = true,
-    desc = 'Delete character without copying',
-})
 
 -- Find and center
 vim.keymap.set('n', 'n', 'nzzzv', {
     noremap = true,
     silent = true,
-    desc = 'Search next occurrence and center',
+    desc = 'search next occurrence and center',
 })
+
 vim.keymap.set('n', 'N', 'Nzzzv', {
     noremap = true,
     silent = true,
-    desc = 'Search previous occurrence and center',
+    desc = 'search previous occurrence and center',
 })
 
 -- Clear highlights on search when pressing <Esc> in normal mode
@@ -69,8 +107,19 @@ vim.keymap.set(
     'n',
     '<Esc>',
     '<cmd>nohlsearch<CR>',
-    { noremap = true, silent = true, desc = 'Clear search highlights' }
+    { noremap = true, silent = true, desc = 'clear search highlights' }
 )
+
+-- -----------------------------
+-- Deleting
+-- -----------------------------
+
+-- Delete single character without copying into register
+vim.keymap.set('n', 'x', '"_x', {
+    noremap = true,
+    silent = true,
+    desc = 'delete character without copying',
+})
 
 -- -----------------------------
 -- Scrolling
@@ -81,13 +130,14 @@ vim.keymap.set(
     'n',
     '<C-d>',
     '<C-d>zz',
-    { noremap = true, silent = true, desc = 'Scroll down and center' }
+    { noremap = true, silent = true, desc = 'scroll down and center' }
 )
+
 vim.keymap.set(
     'n',
     '<C-u>',
     '<C-u>zz',
-    { noremap = true, silent = true, desc = 'Scroll up and center' }
+    { noremap = true, silent = true, desc = 'scroll up and center' }
 )
 
 -- -----------------------------
@@ -99,25 +149,28 @@ vim.keymap.set(
     'n',
     '<Up>',
     ':resize -2<CR>',
-    { noremap = true, silent = true, desc = 'Resize window up' }
+    { noremap = true, silent = true, desc = 'resize window up' }
 )
+
 vim.keymap.set(
     'n',
     '<Down>',
     ':resize +2<CR>',
-    { noremap = true, silent = true, desc = 'Resize window down' }
+    { noremap = true, silent = true, desc = 'resize window down' }
 )
+
 vim.keymap.set(
     'n',
     '<Right>',
     ':vertical resize -2<CR>',
-    { noremap = true, silent = true, desc = 'Resize window right' }
+    { noremap = true, silent = true, desc = 'resize window right' }
 )
+
 vim.keymap.set(
     'n',
     '<Left>',
     ':vertical resize +2<CR>',
-    { noremap = true, silent = true, desc = 'Resize window left' }
+    { noremap = true, silent = true, desc = 'resize window left' }
 )
 
 -- Split and manage windows
@@ -125,25 +178,27 @@ vim.keymap.set(
     'n',
     '<leader>sv',
     '<C-w>v',
-    { noremap = true, silent = true, desc = 'Split window vertically' }
+    { noremap = true, silent = true, desc = '[s]plit window: [v]ertical' }
 )
+
 vim.keymap.set(
     'n',
     '<leader>sh',
     '<C-w>s',
-    { noremap = true, silent = true, desc = 'Split window horizontally' }
+    { noremap = true, silent = true, desc = '[s]plit window: [h]orizontal' }
 )
+
+vim.keymap.set('n', '<leader>se', '<C-w>=', {
+    noremap = true,
+    silent = true,
+    desc = '[s]plit: [e]qual sizes of windows',
+})
+
 vim.keymap.set(
     'n',
-    '<leader>se',
-    '<C-w>=',
-    { noremap = true, silent = true, desc = 'Make split windows equal size' }
-)
-vim.keymap.set(
-    'n',
-    '<leader>sx',
+    '<leader>sc',
     ':close<CR>',
-    { noremap = true, silent = true, desc = 'Close current split window' }
+    { noremap = true, silent = true, desc = '[s]plit: [c]lose' }
 )
 
 -- Navigate between splits
@@ -151,55 +206,26 @@ vim.keymap.set(
     'n',
     '<S-k>',
     ':wincmd k<CR>',
-    { noremap = true, silent = true, desc = 'Move to the window above' }
+    { noremap = true, silent = true, desc = 'move to the window above' }
 )
 vim.keymap.set(
     'n',
     '<S-j>',
     ':wincmd j<CR>',
-    { noremap = true, silent = true, desc = 'Move to the window below' }
+    { noremap = true, silent = true, desc = 'move to the window below' }
 )
 vim.keymap.set(
     'n',
     '<S-h>',
     ':wincmd h<CR>',
-    { noremap = true, silent = true, desc = 'Move to the window on the left' }
+    { noremap = true, silent = true, desc = 'move to the window on the left' }
 )
 vim.keymap.set(
     'n',
     '<S-l>',
     ':wincmd l<CR>',
-    { noremap = true, silent = true, desc = 'Move to the window on the right' }
+    { noremap = true, silent = true, desc = 'move to the window on the right' }
 )
-
--- -----------------------------
--- Tab Management
--- -----------------------------
-
--- vim.keymap.set(
---     'n',
---     '<leader>to',
---     ':tabnew<CR>',
---     { noremap = true, silent = true, desc = 'Open new tab' }
--- )
--- vim.keymap.set(
---     'n',
---     '<leader>tx',
---     ':tabclose<CR>',
---     { noremap = true, silent = true, desc = 'Close current tab' }
--- )
--- vim.keymap.set(
---     'n',
---     '<leader>tn',
---     ':tabn<CR>',
---     { noremap = true, silent = true, desc = 'Go to next tab' }
--- )
--- vim.keymap.set(
---     'n',
---     '<leader>tp',
---     ':tabp<CR>',
---     { noremap = true, silent = true, desc = 'Go to previous tab' }
--- )
 
 -- -----------------------------
 -- Visual Mode Keymaps
@@ -209,47 +235,25 @@ vim.keymap.set(
 vim.keymap.set('v', '<', '<gv', {
     noremap = true,
     silent = true,
-    desc = 'Stay in indent mode when shifting left',
+    desc = 'stay in indent mode when shifting left',
 })
+
 vim.keymap.set('v', '>', '>gv', {
     noremap = true,
     silent = true,
-    desc = 'Stay in indent mode when shifting right',
+    desc = 'stay in indent mode when shifting right',
 })
+
+-- -----------------------------
+-- Clipboard
+-- -----------------------------
 
 -- Keep last yanked when pasting
 vim.keymap.set('v', 'p', '"_dP', {
     noremap = true,
     silent = true,
-    desc = 'Paste without losing last yanked text',
+    desc = 'paste without losing last yanked text',
 })
-
--- -----------------------------
--- Diagnostic Keymaps
--- -----------------------------
-
--- vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, {
---     noremap = true,
---     silent = true,
---     desc = 'Go to previous diagnostic message',
--- })
--- vim.keymap.set(
---     'n',
---     ']d',
---     vim.diagnostic.goto_next,
---     { noremap = true, silent = true, desc = 'Go to next diagnostic message' }
--- )
--- vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, {
---     noremap = true,
---     silent = true,
---     desc = 'Open floating diagnostic message',
--- })
--- vim.keymap.set(
---     'n',
---     '<leader>q',
---     vim.diagnostic.setloclist,
---     { noremap = true, silent = true, desc = 'Open diagnostics list' }
--- )
 
 -- -----------------------------
 -- Buffer Management
@@ -260,21 +264,22 @@ vim.keymap.set(
     'n',
     '<Tab>',
     ':bnext<CR>',
-    { noremap = true, silent = true, desc = 'Go to next buffer' }
+    { noremap = true, silent = true, desc = '[Bufferline]: next buffer' }
 )
+
 vim.keymap.set(
     'n',
     '<S-Tab>',
     ':bprevious<CR>',
-    { noremap = true, silent = true, desc = 'Go to previous buffer' }
+    { noremap = true, silent = true, desc = '[Bufferline]: previous buffer' }
 )
 
 -- Close the current buffer
 vim.keymap.set(
     'n',
-    '<leader>x',
+    '<leader>c',
     ':Bdelete!<CR>',
-    { noremap = true, silent = true, desc = 'Close current buffer' }
+    { noremap = true, silent = true, desc = '[c]lose buffer' }
 )
 
 -- Close all buffers command
@@ -293,15 +298,17 @@ end, { desc = 'Close all buffers' })
 
 vim.keymap.set(
     'n',
-    '<leader>X',
+    '<leader>C',
     ':BdeleteAll<CR>',
-    { noremap = true, silent = true, desc = 'Close all buffers' }
+    { noremap = true, silent = true, desc = '[C]lose all buffers' }
 )
+
+-- Open new buffer
 vim.keymap.set(
     'n',
     '<leader>b',
     '<cmd> new <CR>',
-    { noremap = true, silent = true, desc = 'Open new buffer' }
+    { noremap = true, silent = true, desc = 'open new [b]uffer' }
 )
 
 -- -----------------------------
@@ -312,7 +319,7 @@ vim.keymap.set(
     'n',
     '<leader>e',
     ':Neotree toggle position=left<CR>',
-    { noremap = true, silent = true, desc = 'Toggle file explorer' }
+    { noremap = true, silent = true, desc = 'toggle file [e]xplorer' }
 )
 
 -- -----------------------------
@@ -343,7 +350,7 @@ vim.keymap.set(
     'n',
     '<leader>f',
     ':Format<CR>',
-    { noremap = true, silent = true, desc = 'Format file' }
+    { noremap = true, silent = true, desc = '[f]ormat file' }
 )
 
 -- [[ Basic Autocommands ]]
