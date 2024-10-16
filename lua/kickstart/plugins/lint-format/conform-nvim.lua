23c0614697b115ce921509c6ad1c0ceeddd158c7
masajinobe-ef <priscilla.effects@gmail.com>
2024-10-16 11:43:59 +0500

    up

---
+++ lua/kickstart/plugins/lint-format/conform-nvim.lua
@@ -0,0 +1,96 @@
+return {
+    'stevearc/conform.nvim',
+    event = 'VimEnter',
+    dependencies = { 'mason.nvim' },
+    lazy = true,
+    config = function()
+        require('conform').setup {
+            -- Map of filetype to formatters
+            formatters_by_ft = {
+                lua = { 'stylua' },
+                python = { 'ruff' },
+                sh = { 'shfmt' },
+                go = { 'goimports', 'gofmt' },
+                kotlin = { 'ktlint' },
+                javascript = {
+                    'prettierd',
+                    'prettier',
+                    stop_after_first = true,
+                },
+                typescript = {
+                    'prettierd',
+                    'prettier',
+                    stop_after_first = true,
+                },
+                html = { 'prettierd', 'prettier', stop_after_first = true },
+                css = { 'prettierd', 'prettier', stop_after_first = true },
+                json = { 'prettierd', 'prettier', stop_after_first = true },
+                markdown = { 'prettierd', 'prettier', stop_after_first = true },
+                yaml = { 'prettierd', 'prettier', stop_after_first = true },
+                xml = { 'prettierd', 'prettier', stop_after_first = true },
+                ['*'] = { 'codespell' },
+                ['_'] = { 'trim_whitespace' },
+            },
+
+            -- Custom args
+            formatters = {
+                ruff = {
+                    command = 'ruff',
+                    args = {
+                        'check',
+                        '--fix',
+                        '--use-single-quotes',
+                        '--max-line-length',
+                        '79',
+                        '--indent-width',
+                        '4',
+                        '--indent-style',
+                        'space',
+                        '--ignore',
+                        'F403',
+                        '$FILENAME',
+                    },
+                    stdin = true,
+                    exit_codes = { 0, 1 },
+                },
+                -- stylua = {
+                --     command = 'stylua',
+                --     args = {
+                --         '--column-width',
+                --         '79',
+                --         '--line-endings',
+                --         'Unix',
+                --         '--indent-type',
+                --         'Spaces',
+                --         '--indent-width',
+                --         '4',
+                --         '--quote-style',
+                --         'AutoPreferSingle',
+                --         '--call-parentheses',
+                --         'None',
+                --         '$FILENAME',
+                --     },
+                --     stdin = true,
+                --     exit_codes = { 0, 1 },
+                -- },
+                gofmt = {
+                    command = 'gofmt',
+                    args = { '$FILENAME' },
+                    stdin = false,
+                    exit_codes = { 0 },
+                },
+
+                -- Set default options for formatters
+                default_format_opts = {
+                    lsp_format = 'fallback',
+                    timeout_ms = 500,
+                },
+
+                -- Log level and notifications
+                log_level = vim.log.levels.ERROR,
+                notify_on_error = true,
+                notify_no_formatters = true,
+            },
+        }
+    end,
+}
