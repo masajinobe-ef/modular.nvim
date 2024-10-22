33d1838cb659a645f7c62e8bfa5e798b0288f458
masajinobe-ef <priscilla.effects@gmail.com>
2024-10-22 23:32:47 +0500

    up

--- lua/kickstart/plugins/ui/gitsigns.lua
+++ lua/kickstart/plugins/ui/gitsigns.lua
@@ -31,7 +31,7 @@ return {
             current_line_blame = true,
             current_line_blame_opts = {
                 virt_text = true,
-                virt_text_pos = 'right_align',
+                virt_text_pos = 'eol',
                 delay = 100,
                 ignore_whitespace = false,
                 virt_text_priority = 100,
